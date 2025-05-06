5bb75459892be986af2219c3212cf7a6960e70d6
goodxgrief <samed54@hotmail.de>
2025-05-06 17:23:18 +0200

    changed to decap cms nmp package

---
+++ src/admin.js
@@ -0,0 +1,6 @@
+import CMS from 'decap-cms-app';
+import uploadCare from 'decap-cms-media-library-uploadcare';
+
+CMS.registerMediaLibrary(uploadCare);
+
+// hier ggf. eigene Widgets, Preview Templates etc.
